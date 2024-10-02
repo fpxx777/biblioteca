c537f4d55e151f3d127fceab0e4d56ffc7a38f0b
MeLaZaco <frank.rojas.arbayza@alumnos.sip.cl>
2024-10-02 13:31:17 -0300

    asdasd

--- src/static/JS/categories.js
+++ src/static/JS/categories.js
@@ -37,4 +37,24 @@ document.addEventListener('DOMContentLoaded', function() {
     container.classList.add('hidden2');
 });
 
-
+/*
+btn.addEventListener('click', function () {
+  
+  if (box.classList.contains('hidden')) {
+    box.classList.remove('hidden');
+    setTimeout(function () {
+      box.classList.remove('visuallyhidden');
+    }, 20);
+  } else {
+    box.classList.add('visuallyhidden');    
+    box.addEventListener('transitionend', function(e) {
+      box.classList.add('hidden');
+    }, {
+      capture: false,
+      once: true,
+      passive: false
+    });
+  }
+  
+}, false);
+*/
