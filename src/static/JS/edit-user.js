5af330e93483e0ef7d3a3a01ed742393b8a75f91
MeLaZaco <frank.rojas.arbayza@alumnos.sip.cl>
2024-10-22 12:58:42 -0300

    adasd

--- src/static/JS/edit-user.js
+++ src/static/JS/edit-user.js
@@ -1,19 +1,19 @@
 document.querySelector('.edit-button').addEventListener('click', function () {
-    document.querySelectorAll('.edit-user label, .foto-user label').forEach(label => label.style.display = 'none');
-    document.querySelectorAll('.edit-user input, .foto-user input').forEach(input => input.style.display = 'block');
     document.querySelector('.edit-button').style.display = 'none';
     document.querySelector('.x').style.display = 'flex';
     document.querySelector('.enviar').style.display = 'flex';
     document.getElementById('icon-gmail').style.display = 'none';
     document.getElementById('icon-date').style.display = 'none';
+    document.getElementById('centrar-user').classList.add('move-center');
+    document.getElementById('centrar-user').classList.remove('move-left');
 });
 
 document.querySelector('.x').addEventListener('click', function () {
-    document.querySelectorAll('.edit-user label, .foto-user label').forEach(label => label.style.display = 'block');
-    document.querySelectorAll('.edit-user input, .foto-user input').forEach(input => input.style.display = 'none');
     document.querySelector('.edit-button').style.display = 'flex';
     document.querySelector('.x').style.display = 'none';
     document.querySelector('.enviar').style.display = 'none';
     document.getElementById('icon-gmail').style.display = 'block';
     document.getElementById('icon-date').style.display = 'block';
+    document.getElementById('centrar-user').classList.add('move-left');
+    document.getElementById('centrar-user').classList.remove('move-center');    
 });
