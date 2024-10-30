444646fce09d98346774ee9b438f2e8fb22b4715
MeLaZaco <frank.rojas.arbayza@alumnos.sip.cl>
2024-10-30 03:49:56 -0300

    nijn

--- src/app.py
+++ src/app.py
@@ -181,5 +181,17 @@ def login():
         session["username"] = user.nombre
         return redirect("/")
     
+@app.route("/request/", methods=["GET"])
+def request():
+    return render_template("new_book.html")
+
+@app.route("/request/", methods=["POST"])
+def request_send():
+    title = request.form.get('title')
+    author = request.form.get('author')
+    isbn = request.form.get('isbn')
+    year = request.form.get('year')
+    reason = request.form.get('reason')
+    
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
